62b4adf578e33441b115fa8cde554b0b90c79f4d
Daniel Silverio <daniel.silverio@mariachi.io>
2022-05-12 12:31:33 -0500

    Se agregaron los componentes para el header y el boton que abre el contenedor para agregar un nuevo tweet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Snackbar } from "@material-ui/core";
+import Header from "./components/Header";
+import SendTweet from "./components/SendTweet";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Container className="tweets-simulator" maxWidth={false}>
+    <Header />
+    <SendTweet />
+  </Container>;
 }
 
 export default App;
