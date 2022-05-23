3c78bb16ac34cb340d778a89fde0f1e1ee8e072a
Daniel Silverio <daniel.silverio@mariachi.io>
2022-05-23 11:37:00 -0500

    Se agrego el proceso para poder escribir tweets y mostrarlos en la pantalla

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
+import React, { useState, useEffect } from "react";
 import { Container, Snackbar } from "@material-ui/core";
 import Header from "./components/Header";
 import SendTweet from "./components/SendTweet";
+import { TWEETS_STORAGE } from "./utils/constants";
+import ListTweets from "./components/ListTweets";
 
 function App() {
-  return <Container className="tweets-simulator" maxWidth={false}>
-    <Header />
-    <SendTweet />
-  </Container>;
+  const [toastProps, setToastProps] = useState({ open: false, text: null });
+  const [allTweets, setAllTweets] = useState([]);
+  const [reloadTweets, setReloadTweets] = useState(false);
+
+  useEffect(() => {
+    const AllTweetsStorage = localStorage.getItem(TWEETS_STORAGE);
+    const allTweetsArray = JSON.parse(AllTweetsStorage);
+    setAllTweets(allTweetsArray);
+    setReloadTweets(false);
+  }, [toastProps, reloadTweets]);
+
+  const deleteTweets = (index) => {
+    allTweets.splice(index, 1);
+    setAllTweets(allTweets);
+    localStorage.setItem(TWEETS_STORAGE, JSON.stringify(allTweets));
+    setToastProps({
+      open: true,
+      text: "Tweet eliminado.",
+    });
+    setReloadTweets(true);
+  };
+
+  return (
+    <Container className="tweets-simulator" maxWidth={false}>
+      <Header />
+      <SendTweet setToastProps={setToastProps} allTweets={allTweets} />
+      <ListTweets allTweets={allTweets} deleteTweets={deleteTweets} />
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={toastProps.open}
+        autoHideDuration={2000}
+        onClose={() =>
+          setToastProps({
+            open: false,
+            text: "",
+          })
+        }
+        message={<span id="message-id">{toastProps.text}</span>}
+      />
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/ListTweets/ListTweets.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import "./ListTweets.scss";
+import Tweet from "../Tweet";
+
+export default function ListTweets(props) {
+  const { allTweets, deleteTweets } = props;
+
+  if (!allTweets || allTweets.length === 0) {
+    return (
+      <div className="list-tweets-empty">
+        <h1>No hay Tweets...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <Grid container spacing={3} className="list-tweets">
+      {allTweets.map((tweet, index) => (
+        <Grid key={index} item xs={4}>
+          <Tweet tweet={tweet} index={index} deleteTweets={deleteTweets} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
